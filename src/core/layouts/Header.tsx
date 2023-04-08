9203130aa91a5fc423747061392adf14e81d6e5f
Darya Syomina <d.semina10@gmail.com>
2023-04-08 11:51:48 +0300

    header

--- src/core/layouts/Header.tsx
+++ src/core/layouts/Header.tsx
@@ -1,9 +1,10 @@
 import { useSession } from "@blitzjs/auth"
 import { Routes } from "@blitzjs/next"
 import { useMutation } from "@blitzjs/rpc"
-import { Container, Header as MantineHeader, Text, Group, Button } from "@mantine/core"
+import { Container, Header as MantineHeader, Text, Group, Button, Avatar } from "@mantine/core"
 import Link from "next/link"
 import logout from "src/auth/mutations/logout"
+import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 import Navigation from "./Navigation"
 
 const Header = () => {
@@ -15,15 +16,18 @@ const Header = () => {
     <MantineHeader height={60}>
       <Container size="xl" h="100%">
         <Group position="apart" h="100%">
-          <Group>
-            <Text>Template</Text>
-            <Navigation />
-          </Group>
+          <Text component={Link} href={Routes.Home()}>
+            Template
+          </Text>
+          <Navigation />
           <Group>
             {session.userId ? (
-              <Button size="xs" onClick={() => logoutMutation()}>
-                Выйти
-              </Button>
+              <>
+                <Button size="xs" onClick={() => logoutMutation()}>
+                  Выйти
+                </Button>
+                <Avatar component={Link} href={Routes.ProfilePage()} />
+              </>
             ) : (
               <>
                 <Button size="xs" variant="outline" component={Link} href={Routes.LoginPage()}>
--- src/core/layouts/Navigation.tsx
+++ src/core/layouts/Navigation.tsx
@@ -1,23 +1,54 @@
 import { Routes } from "@blitzjs/next"
-import { Group, Button } from "@mantine/core"
+import { createStyles, Group, Text } from "@mantine/core"
 import Link from "next/link"
 
+const useStyles = createStyles((theme) => ({
+  link: {
+    color: theme.colors[theme.primaryColor][5],
+    borderBottom: "1px solid transparent",
+    transition: "all 0.2s ease",
+    ":hover": {
+      borderBottom: `1px solid ${theme.colors[theme.primaryColor][5]}`,
+    },
+  },
+}))
+
+const navigationItems = [
+  {
+    label: "Объекты",
+    route: Routes.EntitiesPage(),
+  },
+  {
+    label: "Календарь",
+    route: Routes.CalendarPage(),
+  },
+  {
+    label: "Решения",
+    route: Routes.SolutionsPage(),
+  },
+  {
+    label: "Дашборд",
+    route: Routes.DashboardPage(),
+  },
+]
+
 const Navigation = () => {
+  const { classes } = useStyles()
+
   return (
     <nav>
-      <Group spacing="xs">
-        <Button size="xs" component={Link} href={Routes.EntitiesPage()}>
-          Объекты
-        </Button>
-        <Button size="xs" component={Link} href={Routes.CalendarPage()}>
-          Календарь
-        </Button>
-        <Button size="xs" component={Link} href={Routes.SolutionsPage()}>
-          Решения
-        </Button>
-        <Button size="xs" component={Link} href={Routes.DashboardPage()}>
-          Дэшборд
-        </Button>
+      <Group spacing="xl">
+        {navigationItems.map((navItem) => (
+          <Text
+            key={navItem.label}
+            size="sm"
+            component={Link}
+            href={navItem.route}
+            className={classes.link}
+          >
+            {navItem.label}
+          </Text>
+        ))}
       </Group>
     </nav>
   )
